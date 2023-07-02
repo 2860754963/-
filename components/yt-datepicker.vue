a03387bb624b92723a72b84ca735e5c6cf932178
2860754963 <2860754963@qq.com>
2023-07-02 18:38:10 +0800

    '完成油与非油联量明细'

---
+++ components/yt-datepicker.vue
@@ -0,0 +1,107 @@
+<template>
+	<view class="main" :style="{'--width':width,'--height':height}" @click="show = true">
+		<view style="display: flex;height: 50rpx;">
+			<u-icon name="calendar" :size="iconSize"></u-icon>
+			<view style="display: flex;flex-direction: column;justify-content: center;">
+				<input :placeholder="inputPlaceholder" :value="inputValue" disabled></input>
+			</view>
+		</view>
+		<u-datetime-picker id="datePicker" :show="show" v-model="value1" :mode="datetimeMode" :title='dateTitle'
+			@confirm='dateConfirm' @cancel='dateCancel' @change='dateChange'></u-datetime-picker>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+
+			datetimeMode: {
+				type: String,
+				default: 'date',
+				required: false
+			},
+			inputPlaceholder: {
+				type: String,
+				default: '请选择',
+				required: false
+			},
+			dateTitle: {
+				type: String,
+				default: '选择时间',
+				required: false
+			},
+			iconSize: {
+				type: Number,
+				default: 30,
+				required: false
+			},
+			width: {
+				type: String,
+				default: '240rpx',
+				required: false
+			},
+			height: {
+				type: String,
+				default: '55rpx',
+				required: false
+			}
+		},
+		data() {
+			return {
+				show: false,
+				value1: Number(new Date()),
+				inputValue: ''
+			}
+		},
+		methods: {
+
+			dateConfirm(e) {
+				let dateRes = this.result(e.value, e.mode)
+				this.inputValue = dateRes
+				this.$emit('datePickerConfirm', this.inputValue)
+				this.show = false
+
+			},
+			dateCancel() {
+				this.show = false
+			},
+			dateChange(value, mode) {
+				console.log('值改变了', value);
+			},
+			result(time, mode) {
+				const timeFormat = uni.$u.timeFormat
+				switch (mode) {
+					case 'datetime':
+						return timeFormat(time, 'yyyy-mm-dd hh:MM')
+					case 'date':
+						return timeFormat(time, 'yyyy-mm-dd')
+					case 'year-month':
+						return timeFormat(time, 'yyyy-mm')
+					case 'time':
+						return time
+					default:
+						return ''
+				}
+			},
+			appendDomToRoot(el, selector) {
+				const query = this.createSelectorQuery()
+				let container = query.select(selector)
+				container.appendChild(el);
+			}
+		},
+		mounted() {
+			// const query = this.createSelectorQuery()
+			// this.appendDomToRoot(query.select("#datePicker"), "#oilMain");
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+	.main {
+		border: 1px solid #ccc;
+		background-color: #fff;
+		width: var(--width);
+		height: var(--height);
+	}
+</style>
--- pages/oilDetails/oilDetails.vue
+++ pages/oilDetails/oilDetails.vue
@@ -1,17 +1,17 @@
 <template>
-	<view>
+	<view id="oilMain">
 		<view id="navbarcus">
 			<u-navbar height="50" placeholder bgColor="#4989ff" left-text="油品联量明细" left-text-color='#fff' autoBack
 				leftIconColor='#fff'>
 			</u-navbar>
 		</view>
-		<view class="main" :style="{'--height':height}">
-			<view class="top">
+		<view class="main" :style="{'--navbarcusheight':navbarcusheight}">
+			<view class="top" id="top">
 				<view class="toptop">
 					<view style="display: flex;">
 						<u-icon name="order" color="#74b4b3" style="line-height: 80rpx;margin-right: 10rpx;"></u-icon>
 						<view>
-							油品联量明细统计
+							非油品联量明细统计
 						</view>
 					</view>
 					<view style="display: flex;">
@@ -35,11 +35,45 @@
 				</view>
 			</view>
 			<view class="bottomlist">
-				<view class="bottomtop">
-
+				<view class="bottomtop" id="bottomtop">
+					<view class="leftyuan"></view>
+					<view class="rightyuan"></view>
+					<view style="display: flex;justify-content: space-evenly;">
+						<ytdatepicker @datePickerConfirm='e=>starTime=e' inputPlaceholder='开始时间'></ytdatepicker>
+						<ytdatepicker @datePickerConfirm='e=>endTime=e' inputPlaceholder='结束时间'></ytdatepicker>
+					</view>
+					<button type="primary" class="buttonSearch">查询 </button>
 				</view>
-				<view class="resultlist">
-
+				<view class="resultlist"
+					:style="{'--topheight':topheight,'--bottomtopheight':bottomtopheight,'--navbarcusheight':navbarcusheight}">
+					<scroll-view scroll-y="true" style="height: 100%;" @scrolltolower='loadingMore'>
+						<view class="resultItem" v-for="(item,index) in resultitem" :key='index'>
+							<view style="display: flex;flex-direction: column;justify-content: space-evenly;">
+								<view style="display: flex;justify-content: space-around; width: 260rpx;">
+									<view>
+										0#
+									</view>
+									<view class="">
+										枪号：8
+									</view>
+								</view>
+								<view class="">
+									2023-06-20 13:20:31
+								</view>
+							</view>
+							<view style="display: flex;flex-direction: column;justify-content: space-evenly;">
+								<view class="">
+									销量：139.34
+								</view>
+								<view class="">
+									计提：1939.34
+								</view>
+							</view>
+						</view>
+						<view style="height: 50rpx;line-height: 50rpx;">
+							<u-divider text="我是有底线的" textColor="#2979ff" lineColor="#2979ff"></u-divider>
+						</view>
+					</scroll-view>
 				</view>
 			</view>
 		</view>
@@ -47,10 +81,19 @@
 </template>
 
 <script>
+	import ytdatepicker from 'components/yt-datepicker.vue'
 	export default {
+		components: {
+			ytdatepicker
+		},
 		data() {
 			return {
-				height: 0,
+				bottomlistheight: 0,
+				bottomtopheight: 0,
+				navbarcusheight: 0,
+				topheight: 0,
+				starTime: '',
+				endTime: '',
 				goodsArr: [{
 						price: '230.5',
 						thumbnail: '121'
@@ -83,39 +126,106 @@
 						price: '251.5',
 						thumbnail: '12.365'
 					},
-
 				],
+				resultitem: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
 			};
 		},
+		methods: {
+			loadingMore(e) {
+				console.log('到达底部加载更多');
+			},
+			datePickerConfirm(e) {
+				console.log(e, "eeeeee");
+			}
+		},
 		mounted() {
+			const query = wx.createSelectorQuery()
+			query.select('#navbarcus').boundingClientRect((result) => {
+				this.navbarcusheight = result.height + 'px'
+			}).exec()
+			query.select('#top').boundingClientRect((result) => {
+				this.topheight = result.height + 'px'
+			}).exec()
 
+			query.select('#bottomtop').boundingClientRect((result) => {
+				this.bottomtopheight = result.height + 'px'
+			}).exec()
 
 		},
 
-		onLoad: function() {
-			// 获取顶部navbar的高度 用于css计算
-			const query = wx.createSelectorQuery()
-			query.select('#navbarcus').boundingClientRect((result) => {
-				this.height = result.height + 'px'
-			}).exec()
-
-
-		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.resultlist {
+		height: calc(100vh - var(--navbarcusheight) - var(--topheight) - var(--bottomtopheight) - 20px);
+	}
+
+	.resultItem {
+		height: 140rpx;
+		background-color: #fff;
+		display: flex;
+		justify-content: space-between;
+		padding: 0 40rpx;
+		border-bottom: 1px solid #ccc;
+	}
+
+	.resultlist .resultItem:last-child {
+		border-bottom: none;
+	}
+
+	.rightyuan {
+		position: absolute;
+		width: 30rpx;
+		height: 30rpx;
+		border-radius: 50%;
+		background-color: #f4f4f4;
+		right: -16rpx;
+		bottom: -18rpx;
+		z-index: 9999;
+	}
+
+	.leftyuan {
+		position: absolute;
+		width: 30rpx;
+		height: 30rpx;
+		border-radius: 50%;
+		background-color: #f4f4f4;
+		left: -16rpx;
+		top: 184rpx;
+		z-index: 9999;
+	}
+
+	.buttonSearch {
+		width: 97%;
+		background-color: #4989ff;
+		// height: 76rpx;
+		// line-height: 76rpx;
+		font-size: 30rpx;
+		margin-top: 10rpx;
+	}
+
 	.bottomlist {
 		position: relative;
 		top: 20rpx;
-		height: 70%;
+		height: calc(100vh - var(--topheight) - var(--navbarcusheight) - 20px);
 		left: 50%;
 		transform: translateX(-50%);
 		background-color: #fff;
 		width: 92%;
 
 		.bottomtop {
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-evenly;
 			border-bottom: 1px dashed #ccc;
+			height: 200rpx;
+			width: 100%;
+			box-sizing: border-box;
+			padding: 40rpx;
+			padding-bottom: 20rpx;
+
 		}
 	}
 
@@ -135,8 +245,8 @@
 	}
 
 	.main {
-		background-color: pink;
-		height: calc(100vh - var(--height));
+		background-color: #f4f4f4;
+		height: calc(100vh - var(--navbarcusheight));
 	}
 
 
