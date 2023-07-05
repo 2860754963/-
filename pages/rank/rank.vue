32831995408bb991c4af2f6efc7011cb36648d10
2860754963 <2860754963@qq.com>
2023-07-05 17:25:55 +0800

    '排名页面'

--- pages/rank/rank.vue
+++ pages/rank/rank.vue
@@ -1,20 +1,27 @@
 <template>
 	<view>
 		<view>
-			<u-navbar height="50" placeholder bgColor="#4989ff" left-text="评比排名" left-text-color='#fff' autoBack
+			<u-navbar height="50" placeholder bgColor="#5b89fd" left-text="评比排名" left-text-color='#fff' autoBack
 				leftIconColor='#fff'>
 			</u-navbar>
 		</view>
-		<view class="topbg" style="background-image: url('../../static/rankbg.png');">
-			<view>排行榜</view>
-			<view>
-				<view>
-					更新时间
+		<view class="topbg">
+			<view class="ranktext">排行榜</view>
+			<view class="ranktime">
+				<view style="margin-right: 5rpx;">
+					更新时间：
 				</view>
 				<view>
 					2023-06-20
 				</view>
 			</view>
+			<view class="topimage">
+				<img src="https://s1.ax1x.com/2023/07/05/pCyEnWq.png" alt="pCyEnWq.png" border="0"
+					style="width: 350rpx;height: 250rpx;" />
+			</view>
+		</view>
+		<view class="bottomPannel">
+
 		</view>
 	</view>
 </template>
@@ -25,14 +32,43 @@
 			return {
 
 			};
+		},
+		mounted() {
+			const query = wx.createSelectorQuery()
+			query.select('#navbarcus').boundingClientRect((result) => {
+				this.navbarcusheight = result.height + 'px'
+			}).exec()
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.topimage {
+		float: right;
+		margin-top: -210rpx;
+
+	}
+
+	.ranktime {
+		display: flex;
+		padding-left: 50rpx;
+		color: #fff;
+		font-size: 30rpx;
+	}
+
+	.ranktext {
+		padding: 50rpx;
+		margin-bottom: 10rpx;
+		color: #fff;
+		font-size: 90rpx;
+		line-height: 80rpx;
+		font-weight: 800;
+	}
+
 	.topbg {
 		width: 100%;
-		height: 20%;
-		background-color: #4989ff;
+		height: 361rpx;
+		// background: linear-gradient(to right );
+		background-color: #5b89fd;
 	}
 </style>
